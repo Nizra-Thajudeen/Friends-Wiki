9e885eebb187d078fc0ae5d40bc6384e8d909cd6
Nizra-Thajudeen <fnizra09@gmail.com>
2022-05-15 23:19:29 +0530

    added characters api

---
+++ FriendsWiki.Website/Controllers/CharactersController.cs
@@ -0,0 +1,24 @@
+﻿using FriendsWiki.Website.Models;
+using FriendsWiki.Website.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendsWiki.Website.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CharactersController : ControllerBase
+    {
+        public CharactersController(JsonFileCharacterService characterService)
+        {
+            CharacterService = characterService;
+        }
+
+        public JsonFileCharacterService CharacterService { get;} 
+        
+        public IEnumerable<Character> Get()
+        {
+            return CharacterService.GetCharacters();
+        }
+    }
+}
