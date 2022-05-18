fb4fb5ba4e7b4b502bb30c90a4d0441819838234
Nizra-Thajudeen <fnizra09@gmail.com>
2022-05-18 22:11:43 +0530

    added 'more infor' modal

---
+++ FriendsWiki.Website/Pages/CharacterDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FriendsWiki.Website.Pages
+{
+    public class CharacterDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
